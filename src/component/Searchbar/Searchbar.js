6d71b570d8265f983261d7a6b1b0c90a2c685a93
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-12-19 02:57:16 +0200

    searchBar add

--- src/component/Searchbar/Searchbar.js
+++ src/component/Searchbar/Searchbar.js
@@ -1,48 +1,43 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { ReactComponent as Logo } from "../../logo.svg";
 import s from "./Searchbar.module.css";
-class Search extends Component {
-  state = {
-    imgName: "",
-  };
-  searchPhoto = (e) => {
-    this.setState({ imgName: e.target.value.toLowerCase() });
+function Search({ img }) {
+  const [imgName, setImgName] = useState("");
+
+  const searchPhoto = (e) => {
+    setImgName(e.target.value.toLowerCase());
   };
 
-  handleSubmit = (el) => {
-    const { imgName } = this.state;
+  const handleSubmit = (el) => {
     el.preventDefault();
     if (imgName.trim() === "") {
       return;
     }
-    this.props.img(imgName);
-    this.setState({ imgName: "" });
+    img(imgName);
+    setImgName("");
   };
-  render() {
-    const { imgName } = this.state;
 
-    return (
-      <>
-        <header className={s.form__block}>
-          <form className={s.form} onSubmit={this.handleSubmit}>
-            <button className={s.button__submit} type="submit">
-              <span>
-                <Logo width="30px" height="30px" />
-              </span>
-              <span className={s.button__label}>Search</span>
-            </button>
-            <input
-              onChange={this.searchPhoto}
-              className={s.input}
-              type="text"
-              placeholder="Search images and photos"
-              value={imgName}
-            />
-          </form>
-        </header>
-      </>
-    );
-  }
+  return (
+    <>
+      <header className={s.form__block}>
+        <form className={s.form} onSubmit={handleSubmit}>
+          <button className={s.button__submit} type="submit">
+            <span>
+              <Logo width="30px" height="30px" />
+            </span>
+            <span className={s.button__label}>Search</span>
+          </button>
+          <input
+            onChange={searchPhoto}
+            className={s.input}
+            type="text"
+            placeholder="Search images and photos"
+            value={imgName}
+          />
+        </form>
+      </header>
+    </>
+  );
 }
 
 export default Search;
